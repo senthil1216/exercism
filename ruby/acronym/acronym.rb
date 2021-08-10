106a026fb228e331f59fba8f828df470b1bb6c0f
senthil sivasubramanian <senthil1216@gmail.com>
2021-08-09 22:49:06 -0700

    solved acronym, microwave

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -2,8 +2,8 @@ class Acronym
   def self.abbreviate(input)
     out = input
             .split(/\W+/)
-            .map{ |w| w[0] }
-            .join("")
+            .map(&:chr)
+            .join
             .upcase
  end
 end
--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,7 +1,15 @@
-=begin
-Write your code for the 'Microwave' exercise in this file. Make the tests in
-`microwave_test.rb` pass.
+class Microwave
+  def initialize(input)
+    @input = input
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/microwave` directory.
-=end
+  def timer
+    if @input >= 100
+      seconds = (@input / 100 * 60) + (@input % 100)
+    else
+      seconds = @input
+    end
+
+    sprintf("%02d:%02d", seconds / 60, seconds % 60)
+  end
+end
